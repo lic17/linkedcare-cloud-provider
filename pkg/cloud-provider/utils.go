3b248a23bc0d2f9fb9e07cd45664138303188978
cheng.li <cheng1.li@linkedcare.cn>
2020-02-26 15:12:25 +0800

    fix err of create loadbalance

--- pkg/cloud-provider/utils.go
+++ pkg/cloud-provider/utils.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/errors"
 	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/slb"
 	v1 "k8s.io/api/core/v1"
@@ -45,58 +46,109 @@ func BuildVirturalGroupFromService(s *LoadBalancerClient, service *v1.Service, n
 
 	/*Create Vserver Group for nodes*/
 	for _, port := range service.Spec.Ports {
-		create_vsg_request := slb.CreateCreateVServerGroupRequest()
-		create_vsg_request.VServerGroupName = service.Namespace + "-" + service.Name
+		vsg_name := service.Namespace + "-" + service.Name + "-" + strconv.Itoa(int(port.NodePort))
+		hadVsg := false
+		hadListener := false
 
-		var servers []VsgBackendServer
-		for i, node := range nodes {
-			if i < 5 {
-				server := new(VsgBackendServer)
-				nodeid, err := nodeFromProviderID(node.Spec.ProviderID)
-				if err != nil {
-					fmt.Print(err.Error())
+		//first to get vsg
+		get_vsg_request := slb.CreateDescribeVServerGroupsRequest()
+		//get_vsg_request.Scheme = "https"
+		get_vsg_request.LoadBalancerId = lbsId
+
+		get_vsg_response, err := s.c.DescribeVServerGroups(get_vsg_request)
+		if err != nil {
+			fmt.Print(err.Error())
+			return err
+		}
+		for _, vsg := range get_vsg_response.VServerGroups.VServerGroup {
+			if !hadVsg {
+				if vsg.VServerGroupName == vsg_name {
+					hadVsg = true
 					break
 				}
-
-				server.ServerId = nodeid
-				server.Weight = "100"
-				server.Type = "ecs"
-				server.Port = strconv.Itoa(int(port.NodePort))
-				server.Description = "tcp-" + server.Port
-				servers = append(servers, *server)
 			}
 		}
-		jsonServers, _ := json.Marshal(servers)
-		//create_vsg_request.Scheme = "https"
 
-		create_vsg_request.BackendServers = string(jsonServers)
+		vsgId := ""
 
-		//create_vsg_request.BackendServers = "[{ \"ServerId\": \"i-xxxxxxxxx\", \"Weight\": \"100\", \"Type\": \"ecs\", \"Port\":\"80\",\"Description\":\"test-112\" }]"
-		create_vsg_request.LoadBalancerId = lbsId
+		//if don't  have vsg to create vsg
+		if !hadVsg {
+			create_vsg_request := slb.CreateCreateVServerGroupRequest()
+			create_vsg_request.VServerGroupName = vsg_name
 
-		vsg_response, err := s.c.CreateVServerGroup(create_vsg_request)
+			var servers []VsgBackendServer
+			for i, node := range nodes {
+				if i < 5 {
+					server := new(VsgBackendServer)
+					nodeid, err := nodeFromProviderID(node.Spec.ProviderID)
+					if err != nil {
+						fmt.Print(err.Error())
+						break
+					}
+
+					server.ServerId = nodeid
+					server.Weight = "100"
+					server.Type = "ecs"
+					server.Port = strconv.Itoa(int(port.NodePort))
+					server.Description = "tcp-" + server.Port
+					servers = append(servers, *server)
+				}
+			}
+			jsonServers, _ := json.Marshal(servers)
+			//create_vsg_request.Scheme = "https"
+
+			create_vsg_request.BackendServers = string(jsonServers)
+
+			//create_vsg_request.BackendServers = "[{ \"ServerId\": \"i-xxxxxxxxx\", \"Weight\": \"100\", \"Type\": \"ecs\", \"Port\":\"80\",\"Description\":\"test-112\" }]"
+			create_vsg_request.LoadBalancerId = lbsId
+
+			vsg_response, err := s.c.CreateVServerGroup(create_vsg_request)
+			if err != nil {
+				fmt.Print(err.Error())
+				return err
+			}
+			vsgId = vsg_response.VServerGroupId
+			fmt.Println(vsg_response)
+		}
+
+		/***************get Listener**********************/
+		listener_request := slb.CreateDescribeLoadBalancerTCPListenerAttributeRequest()
+		//listener_request.Scheme = "https"
+
+		listener_request.ListenerPort = requests.NewInteger(int(port.Port))
+		listener_request.LoadBalancerId = lbsId
+
+		_, err = s.c.DescribeLoadBalancerTCPListenerAttribute(listener_request)
 		if err != nil {
 			fmt.Print(err.Error())
-			return err
+			if clientError, ok := err.(*errors.ClientError); ok {
+				if clientError.ErrorCode() == "ListenerNotFound" {
+					hadListener = false
+				}
+			}
+		} else {
+			hadListener = true
 		}
-		fmt.Println(vsg_response)
 
-		/*Create TCPListener of LoadBalancer*/
-		create_tcp_request := slb.CreateCreateLoadBalancerTCPListenerRequest()
-		//create_tcp_request.Scheme = "https"
+		//if don't  have TCPListener to create TCPListener
+		if !hadListener {
+			/*Create TCPListener of LoadBalancer*/
+			create_tcp_request := slb.CreateCreateLoadBalancerTCPListenerRequest()
+			//create_tcp_request.Scheme = "https"
 
-		create_tcp_request.VServerGroupId = vsg_response.VServerGroupId
-		create_tcp_request.ListenerPort = requests.NewInteger(int(port.Port))
-		create_tcp_request.Bandwidth = requests.NewInteger(-1)
-		create_tcp_request.LoadBalancerId = lbsId
-		create_tcp_request.BackendServerPort = requests.NewInteger(int(port.NodePort))
+			create_tcp_request.VServerGroupId = vsgId
+			create_tcp_request.ListenerPort = requests.NewInteger(int(port.Port))
+			create_tcp_request.Bandwidth = requests.NewInteger(-1)
+			create_tcp_request.LoadBalancerId = lbsId
+			create_tcp_request.BackendServerPort = requests.NewInteger(int(port.NodePort))
 
-		tcp_response, err := s.c.CreateLoadBalancerTCPListener(create_tcp_request)
-		if err != nil {
-			fmt.Print(err.Error())
-			return err
+			tcp_response, err := s.c.CreateLoadBalancerTCPListener(create_tcp_request)
+			if err != nil {
+				fmt.Print(err.Error())
+				return err
+			}
+			fmt.Println(tcp_response)
 		}
-		fmt.Println(tcp_response)
 	}
 	start_request := slb.CreateSetLoadBalancerStatusRequest()
 	//start_request.Scheme = "https"
